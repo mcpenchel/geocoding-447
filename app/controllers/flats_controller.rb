24e761889136bb4c8b729584dfea98479b814dfa
Matheus Penchel <matheus.penchel@gmail.com>
2020-09-02 10:40:42 -0300

    Implemented the infoWindow and added it to the map

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,7 +14,8 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
       }
     end
   end
